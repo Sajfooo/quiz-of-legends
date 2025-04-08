e4ff5dc9994e452f66f6c403c46f37c0ed2c4794
Sajfooo <gszumi.7@gmail.com>
2025-04-08 13:00:06 +0200

    add api call module

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from "react";
 import { useConfig } from "./store/Store";
 import { AbilityTypes } from "./enum/AbilityTypes";
+import { fetchChampionData, fetchChampions, getImageAndData } from "./api/ddragon";
 
 function App() {
     const [message, setMessage] = useState("Hello World!");
     const [imageUrl, setImageUrl] = useState(null);
-
     const { config, updateConfig } = useConfig();
-
     const version = config.ddragon.version;
     const language = config.ddragon.language;
     
@@ -20,58 +19,19 @@ function App() {
     }
 
     function getRandomSkill() {
-        const abilities = Object.values(AbilityTypes);
+        const abilities = Object.values(AbilityTypes); 
         const randomIndex = Math.floor(Math.random() * abilities.length); 
         return abilities[randomIndex]; 
     }
 
     // Przykład: pobranie listy championów z DDragon (opcjonalnie)
     useEffect(() => {
-        async function fetchChampions() {
-            try {
-
-                const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion.json`);
-                const data = await response.json();
-                // console.log("Champions:", Object.keys(data.data)); // wypisz listę championów w konsoli
-                return Object.keys(data.data);
-            } catch (error) {
-                console.error("Error fetching champions:", error);
-            }
-            return null;
-        }
-
-        async function getChampionDetails(championName) {
-            const response = await fetch(`https://ddragon.leagueoflegends.com/cdn/${version}/data/${language}/champion/${championName}.json`);
-            const data = await response.json();
-
-            return data.data;
-        }
-
-        async function getImageAndData (name, details, skill) {
-            console.log(skill);
-            let query = null;
-            if (skill === AbilityTypes.PASSIVE) {
-                const passiveName = details[name].passive.image.full;
-                console.log("passive name: ", passiveName)
-                query = `https://ddragon.leagueoflegends.com/cdn/${version}/img/passive/${passiveName}`;
-
-            } else {
-
-                const skillName = details[name].spells[skill].image.full;
-                console.log("skill name: ", skillName)
-                query = `https://ddragon.leagueoflegends.com/cdn/${version}/img/spell/${skillName}`;
-            }
-            setImageUrl(query);
-
-            return query, {};
-        }
-
-        fetchChampions().then(champions => {
+        fetchChampions(version, language).then(champions => {
             const champion = getRandomElementFromList(champions);
-            getChampionDetails(champion).then(details => {
+            fetchChampionData(champion).then(details => {
                 const randomSkill = getRandomSkill();
-                getImageAndData(champion, details, randomSkill).then(imageurl, data => {
-
+                getImageAndData(champion, details, randomSkill, version).then(([query, data]) => {
+                    setImageUrl(query);
                 });
             });
         });
